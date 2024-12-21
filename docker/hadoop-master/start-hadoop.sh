0f6330f31be44d133d27cc64fe12d919055072d5
Soufiane ISAM <isam.soufiane1@gmail.com>
2024-12-22 00:00:12 +0100

    3rd commit

---
+++ docker/hadoop-master/start-hadoop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$HADOOP_HOME/bin/hdfs namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+tail -f /dev/null
---
+++ docker/hadoop-worker/start-datanode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+$HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
+$HADOOP_HOME/sbin/yarn-daemon.sh start nodemanager
+tail -f /dev/null
